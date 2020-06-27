eb5100eb1dabe5138b4f774076e62fc5af2c7c03
Esteban Gutiérrez Marín <esteban.gutierrez@bestsecret.com>
2020-06-27 22:40:34 +0200

    Initial configuration of the project.

---
+++ src/main/java/com/bestsecret/groupifier/GroupifierApplication.java
@@ -0,0 +1,13 @@
+package com.bestsecret.groupifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroupifierApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bestsecret/groupifier/GroupifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestsecret.groupifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupifierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
