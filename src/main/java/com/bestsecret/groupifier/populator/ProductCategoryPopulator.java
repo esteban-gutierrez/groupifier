f5319a9a5bf78317c75dd66a196ff4de4f3e85b8
Esteban Gutiérrez Marín <esteban.gutierrez@bestsecret.com>
2020-07-08 18:56:53 +0200

    Added logic to set the correct dates.

--- src/main/java/com/bestsecret/groupifier/populator/ProductCategoryPopulator.java
+++ src/main/java/com/bestsecret/groupifier/populator/ProductCategoryPopulator.java
@@ -1,23 +1,41 @@
 package com.bestsecret.groupifier.populator;
 
 import com.bestsecret.groupifier.model.ProductCategoryEntity;
+import com.bestsecret.groupifier.util.TimeService;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 @Service
 public class ProductCategoryPopulator {
+    @Resource
+    private TimeService timeService;
+
     public void populateMainData(ProductCategoryEntity productCategory, ProductCategoryEntity productCategoryMainData)
         throws PopulatorException {
         try {
             productCategory.setName(productCategoryMainData.getName());
             productCategory.setDescription(productCategoryMainData.getDescription());
-            if (productCategoryMainData.getCreatedAt() != null) {
-                productCategory.setCreatedAt(productCategoryMainData.getCreatedAt());
-            }
-            if (productCategoryMainData.getModifiedAt() != null) {
-                productCategory.setCreatedAt(productCategoryMainData.getModifiedAt());
-            }
+            setCreatedAt(productCategory, productCategoryMainData);
+            setModifiedAt(productCategory, productCategoryMainData);
         } catch (Exception e) {
             throw new PopulatorException("Error populating main data of product category", e);
+        }
+    }
+
+    private void setCreatedAt(ProductCategoryEntity productCategory, ProductCategoryEntity productCategoryMainData) {
+        if (productCategoryMainData.getCreatedAt() != null) {
+            productCategory.setCreatedAt(productCategoryMainData.getCreatedAt());
+        } else if (productCategory.getCreatedAt() == null) {
+            productCategory.setCreatedAt(timeService.getCurrentSQLDate());
+        }
+    }
+
+    private void setModifiedAt(ProductCategoryEntity productCategory, ProductCategoryEntity productCategoryMainData) {
+        if (productCategoryMainData.getModifiedAt() != null) {
+            productCategory.setCreatedAt(productCategoryMainData.getModifiedAt());
+        } else {
+            productCategory.setCreatedAt(timeService.getCurrentSQLDate());
         }
     }
 
